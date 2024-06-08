fda554c8a0b8257dad24f7ec48078b6931da6046
Shivang Sharma <sharmashivang1999@gmail.com>
2024-06-08 16:38:07 +0530

    fix: cloudinary url updated

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const CDN_URL =
-  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1024/";
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
 export const LOGO_URL = "https://cdn.worldvectorlogo.com/logos/swiggy-1.svg";
 
