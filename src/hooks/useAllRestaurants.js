caefdb21fea42b3d962e6b9381f1b48dcca45d2c
Shivang Sharma <sharmashivang1999@gmail.com>
2025-03-05 23:30:17 +0530

    fix: added try catch to restaurants api

--- src/hooks/useAllRestaurants.js
+++ src/hooks/useAllRestaurants.js
@@ -23,32 +23,37 @@ const useAllRestaurants = () => {
 
   // function to fetch the data
   const getAllRestaurants = async () => {
-    const data = await fetch(getAllRestaurantAPI(lat, lng));
+    try {
+      const data = await fetch(getAllRestaurantAPI(lat, lng));
 
-    const jsonData = await data.json();
+      const jsonData = await data.json();
 
-    if (
-      jsonData?.data?.communication?.swiggyNotPresent?.swiggyNotPresent === true
-    ) {
-      dispatch(setUnserviceable());
-      return;
-    }
+      if (
+        jsonData?.data?.communication?.swiggyNotPresent?.swiggyNotPresent ===
+        true
+      ) {
+        dispatch(setUnserviceable());
+        return;
+      }
 
-    const restaurantData =
-      jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
-        ?.restaurants; // optional chaining
+      const restaurantData =
+        jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle
+          ?.restaurants; // optional chaining
 
-    const carouselData =
-      jsonData?.data?.cards[0]?.card?.card?.imageGridCards?.info;
+      const carouselData =
+        jsonData?.data?.cards[0]?.card?.card?.imageGridCards?.info;
 
-    if (restaurantData) {
-      dispatch(fillAllRestaurants(restaurantData));
-      dispatch(fillBackupRestaurants(restaurantData));
-      dispatch(putCarouselItems(carouselData));
-    } else {
-      dispatch(fillAllRestaurants([]));
-      dispatch(fillBackupRestaurants([]));
-      dispatch(putCarouselItems([]));
+      if (restaurantData) {
+        dispatch(fillAllRestaurants(restaurantData));
+        dispatch(fillBackupRestaurants(restaurantData));
+        dispatch(putCarouselItems(carouselData));
+      } else {
+        dispatch(fillAllRestaurants([]));
+        dispatch(fillBackupRestaurants([]));
+        dispatch(putCarouselItems([]));
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
 };
