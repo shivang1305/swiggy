a49d4c7d0854aadfa330cfe260ac0ca6db669051
Shivang Sharma <sharmashivang1999@gmail.com>
2025-03-07 12:56:39 +0530

    feat: firebase config file added

---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "swiggy-d58a6.firebaseapp.com",
+  projectId: "swiggy-d58a6",
+  storageBucket: "swiggy-d58a6.firebasestorage.app",
+  messagingSenderId: "265264237069",
+  appId: "1:265264237069:web:16de4a6ac01f5c49d7e344",
+  measurementId: "G-D5F61D0E3R",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
